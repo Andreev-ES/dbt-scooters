b9553dbaf1e30812bc78e04affff81f54ad8c8c5
unknown <eandreev81@mail.ru>
2026-03-20 20:56:05 +0300

    mission8_4

--- models/events_clean.sql
+++ models/events_clean.sql
@@ -1,14 +1,20 @@
+{% set date = var("date", none) %}
 SELECT DISTINCT
     e.user_id
     ,e."timestamp"
     ,e.type_id
     ,{{ updated_at() }}
 FROM {{ source("scooters_raw", "events") }} AS e
+ WHERE
 {% if is_incremental() %}
-    WHERE "timestamp" > (
-        SELECT max(e."timestamp") 
-        FROM {{ this }} AS e
-    )
+    {% if date %}
+        date("timestamp") = date '{{ date }}'
+    {% else %}
+            "timestamp" > (
+            SELECT max(e."timestamp") 
+            FROM {{ this }} AS e
+        )
+    {% endif %}    
 {% else %}
     WHERE e."timestamp" < '2023-08-01'::timestamp 
 {% endif %}
